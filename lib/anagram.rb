58fdf0beb18c356c79a7c99bbc538fcdec6e48ea
GABRIEL PEREZ <gabriel1994.gp@gmail.com>
2019-12-22 01:51:53 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :array, :word 
+  
+  def initialize(array)
+    @array = array
+    @word = word 
+  end
+
+  def match(array)
+    array.select do |word|
+      is_array?(word)
+    end
+  end 
+
+  def is_array?(word)
+    word.chars.sort == array.chars.sort
+  end
+  
+  diaper = Anagram.new("diaper")
+  diaper.match (%w(enlists google dipper banana))
+end
